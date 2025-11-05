215dd53b74ce2e7b768d14b9086076e9ad2b2097
Aach1 <harshithjaiganji@gmail.com>
2025-11-06 02:23:54 +0530

    Migrate BiONet from Keras to PyTorch

--- core/model.py
+++ core/model.py
@@ -1,158 +1,143 @@
-from keras.models import Model, load_model
-from keras.layers import multiply, add, Permute, Reshape, Dense, GlobalAveragePooling2D, BatchNormalization, Conv2D, Conv2DTranspose, MaxPooling2D, UpSampling2D, Input, concatenate, Add, Concatenate
-from keras import backend as K
-import tensorflow as tf
-import keras
+from torch.nn import Module, Sequential, Conv2d, BatchNorm2d, ConvTranspose2d, ReLU, MaxPool2d, Sigmoid, Parameter
+from torch import tensor, cat
 
-from utils import get_augmented
 
-import numpy as np
+class BiONet(Module):
 
-class BiONet():
-    
     def __init__(self,
-                 input_shape,
-                 num_classes=1,
-                 iterations=2,
-                 multiplier=1.0,
-                 activation='relu',
-                 kernel_initializer='he_normal',
-                 padding='same',
-                 kernel_size=(3,3),
-                 num_layers=4,
-                 integrate=False):
-        self.num_layers = num_layers
-        self.input_shape = input_shape
-        self.num_classes = num_classes
+                 num_classes: int = 1,
+                 iterations: int = 3,
+                 multiplier: float = 1.0,
+                 num_layers: int = 4,
+                 integrate: bool = False):
+
+        super(BiONet, self).__init__()
         self.iterations = iterations
         self.multiplier = multiplier
+        self.num_layers = num_layers
         self.integrate = integrate
-        
-        self.filters_list = [32,64,128,256,512]
-        self.bachnorm_momentum = 0.01
-        
-        self.conv_args = {
-            'kernel_size':kernel_size,
-            'activation':activation, 
-            'padding':padding,
-            'kernel_initializer':kernel_initializer
-            }
+        self.batch_norm_momentum = 0.01
+        self.filters_list = [int(32 * (2 ** i) * self.multiplier) for i in range(self.num_layers + 1)]
+        self.pre_transform_conv_block = Sequential(
+            Conv2d(1, self.filters_list[0], kernel_size=(3, 3), padding=(1, 1), stride=(1, 1)),
+            ReLU(),
+            BatchNorm2d(self.filters_list[0], momentum=self.batch_norm_momentum),
+            Conv2d(self.filters_list[0], self.filters_list[0], kernel_size=(3, 3), padding=(1, 1), stride=(1, 1)),
+            ReLU(),
+            BatchNorm2d(self.filters_list[0], momentum=self.batch_norm_momentum),
+            Conv2d(self.filters_list[0], self.filters_list[0], kernel_size=(3, 3), padding=(1, 1), stride=(1, 1)),
+            ReLU(),
+            BatchNorm2d(self.filters_list[0], momentum=self.batch_norm_momentum),
+            MaxPool2d(kernel_size=(2, 2), stride=(2, 2), padding=(0, 0))
+        )
+        self.reuse_convs = []
+        self.encoders = []
+        self.reuse_deconvs = []
+        self.decoders = []
+        for iteration in range(self.iterations):
+            for layer in range(self.num_layers):
 
-        self.convT_args = {
-            'kernel_size':kernel_size,
-            'activation':activation, 
-            'strides':(2,2),
-            'padding':padding
-            }
-        
-        self.maxpool_args = {
-            'pool_size':(2,2),
-            'strides':(2,2),
-            'padding':'valid',
-            }
-        
-    #define reusable layers
-    def define_layers(self):
-        
-        #reuse feature collections
-        conv_layers = []
-        deconv_layers = []
-        mid_layers = []
-        
-        mid_layers.append(Conv2D(int(self.filters_list[self.num_layers] * self.multiplier),**self.conv_args))
-        mid_layers.append(Conv2D(int(self.filters_list[self.num_layers] * self.multiplier),**self.conv_args))
-        mid_layers.append(Conv2DTranspose(int(self.filters_list[self.num_layers] * self.multiplier), **self.convT_args))
+                in_channel = self.filters_list[layer] * 2
+                mid_channel = self.filters_list[layer]
+                out_channel = self.filters_list[layer + 1]
+                if iteration == 0:
+                    conv1 = Conv2d(in_channel, mid_channel, kernel_size=(3, 3), padding=(1, 1), stride=(1, 1))
+                    conv2 = Conv2d(mid_channel, mid_channel, kernel_size=(3, 3), padding=(1, 1), stride=(1, 1))
+                    conv3 = Conv2d(mid_channel, out_channel, kernel_size=(3, 3), padding=(1, 1), stride=(1, 1))
+                    self.reuse_convs.append((conv1, conv2, conv3))
+                convs = Sequential(
+                    self.reuse_convs[layer][0],
+                    ReLU(),
+                    BatchNorm2d(mid_channel, momentum=self.batch_norm_momentum),
+                    self.reuse_convs[layer][1],
+                    ReLU(),
+                    BatchNorm2d(mid_channel, momentum=self.batch_norm_momentum)
+                )
+                down = Sequential(
+                    self.reuse_convs[layer][2],
+                    ReLU(),
+                    BatchNorm2d(out_channel, momentum=self.batch_norm_momentum),
+                    MaxPool2d(kernel_size=(2, 2), stride=(2, 2), padding=(0, 0))
+                )
+                self.add_module("iteration{0}_layer{1}_encoder_convs".format(iteration, layer), convs)
+                self.add_module("iteration{0}_layer{1}_encoder_down".format(iteration, layer), down)
+                self.encoders.append((convs, down))
 
-        for l in range(self.num_layers):
-            conv_layers.append(Conv2D(int(self.filters_list[l] * self.multiplier), **self.conv_args))
-            conv_layers.append(Conv2D(int(self.filters_list[l] * self.multiplier), **self.conv_args))
-            conv_layers.append(Conv2D(int(self.filters_list[l+1] * self.multiplier), **self.conv_args))
+                in_channel = self.filters_list[self.num_layers - layer] + self.filters_list[self.num_layers - 1 - layer]
+                out_channel = self.filters_list[self.num_layers - 1 - layer]
 
-        for l in range(self.num_layers):
-            deconv_layers.append(Conv2D(int(self.filters_list[self.num_layers-1-l] * self.multiplier), **self.conv_args))
-            deconv_layers.append(Conv2D(int(self.filters_list[self.num_layers-1-l] * self.multiplier), **self.conv_args))
-            deconv_layers.append(Conv2DTranspose(int(self.filters_list[self.num_layers-1-l] * self.multiplier), **self.convT_args))
-        
-        return conv_layers, deconv_layers, mid_layers
-        
-    #define O-Net graph, reusable layers will be passed in
-    def define_graph(self, conv_layers, mid_layers, deconv_layers):
-      
-        inputs = Input(self.input_shape)   
+                if iteration == 0:
+                    conv1 = Conv2d(in_channel, out_channel, kernel_size=(3, 3), padding=(1, 1), stride=(1, 1))
+                    conv2 = Conv2d(out_channel, out_channel, kernel_size=(3, 3), padding=(1, 1), stride=(1, 1))
+                    conv3 = ConvTranspose2d(out_channel, out_channel, kernel_size=(3, 3), padding=(1, 1), stride=(2, 2),
+                                            output_padding=(1, 1))
+                    self.reuse_deconvs.append((conv1, conv2, conv3))
 
-        #the first stage block is not reusable
-        x = self.conv_block(inputs, self.bachnorm_momentum, int(32*self.multiplier))
-        shortcut = self.conv_block(x, self.bachnorm_momentum, int(32*self.multiplier))  
-        x = self.conv_block(shortcut, self.bachnorm_momentum, int(32*self.multiplier))
-        x_in = MaxPooling2D(**self.maxpool_args)(x)
+                convs = Sequential(
+                    self.reuse_deconvs[layer][0],
+                    ReLU(),
+                    BatchNorm2d(out_channel, momentum=self.batch_norm_momentum),
+                    self.reuse_deconvs[layer][1],
+                    ReLU(),
+                    BatchNorm2d(out_channel, momentum=self.batch_norm_momentum)
+                )
+                up = Sequential(
+                    self.reuse_deconvs[layer][2],
+                    ReLU(),
+                    BatchNorm2d(out_channel, momentum=self.batch_norm_momentum)
+                )
+                self.add_module("iteration{0}_layer{1}_decoder_convs".format(iteration, layer), convs)
+                self.add_module("iteration{0}_layer{1}_decoder_up".format(iteration, layer), up)
+                self.decoders.append((convs, up))
+        self.middles = Sequential(
+            Conv2d(self.filters_list[-1], self.filters_list[-1], kernel_size=(3, 3), padding=(1, 1), stride=(1, 1)),
+            ReLU(),
+            BatchNorm2d(self.filters_list[-1], momentum=self.batch_norm_momentum),
+            Conv2d(self.filters_list[-1], self.filters_list[-1], kernel_size=(3, 3), padding=(1, 1), stride=(1, 1)),
+            ReLU(),
+            BatchNorm2d(self.filters_list[-1], momentum=self.batch_norm_momentum),
+            ConvTranspose2d(self.filters_list[-1], self.filters_list[-1], kernel_size=(3, 3), padding=(1, 1),
+                            stride=(2, 2), output_padding=(1, 1)),
+            ReLU(),
+            BatchNorm2d(self.filters_list[-1], momentum=self.batch_norm_momentum)
+        )
+        self.post_transform_conv_block = Sequential(
+            Conv2d(self.filters_list[0] * self.iterations, self.filters_list[0], kernel_size=(3, 3), padding=(1, 1),
+                   stride=(1, 1)) if self.integrate else Conv2d(self.filters_list[0],
+                                                                self.filters_list[0], kernel_size=(3, 3),
+                                                                padding=(1, 1), stride=(1, 1)),
+            ReLU(),
+            BatchNorm2d(self.filters_list[0], momentum=self.batch_norm_momentum),
+            Conv2d(self.filters_list[0], self.filters_list[0], kernel_size=(3, 3), padding=(1, 1), stride=(1, 1)),
+            ReLU(),
+            BatchNorm2d(self.filters_list[0], momentum=self.batch_norm_momentum),
+            Conv2d(self.filters_list[0], 1, kernel_size=(1, 1), stride=(1, 1)),
+            Sigmoid(),
+        )
 
-        back_layers = []
-        collection = []
- 
-
-        for it in range(self.iterations):
-
-            #down layers to carry forward skip connections
-            down_layers = []
-
-            for l in range(self.num_layers):
-                if l == 0:
-                    x = x_in
-
-                if len(back_layers) != 0:
-                    x = Concatenate()([x,back_layers[-1-l]])
-                else:
-                    x = Concatenate()([x,x])
-                    
-                x = self.conv_block(x, self.bachnorm_momentum, conv = conv_layers[3*l])
-                x = self.conv_block(x, self.bachnorm_momentum, conv = conv_layers[3*l+1])
-                down_layers.append(x)
-                x = self.conv_block(x, self.bachnorm_momentum, conv = conv_layers[3*l+2])
-                x = MaxPooling2D(**self.maxpool_args)(x)
-                
-            #back layers to carry backward skip connections, refresh in each inference iteration
-            back_layers = []
-
-            x = self.conv_block(x, self.bachnorm_momentum, conv=mid_layers[0])
-            x = self.conv_block(x, self.bachnorm_momentum, conv=mid_layers[1])
-            x = self.conv_block(x, self.bachnorm_momentum, conv=mid_layers[2])
-
-            for l in range(self.num_layers):        
-                x = concatenate([x, down_layers[-1-l]])  
-                x = self.conv_block(x, self.bachnorm_momentum, conv = deconv_layers[3*l])
-                x = self.conv_block(x, self.bachnorm_momentum, conv = deconv_layers[3*l+1])
-                back_layers.append(x)
-                
-                x = self.conv_block(x, self.bachnorm_momentum, conv = deconv_layers[3*l+2])
-                #integrate decoded features
-                if l == self.num_layers - 1 and self.integrate:
-                    collection.append(x)
-        
-        #to use integrate or not
+    def forward(self, x: tensor) -> tensor:
+        enc = [None for i in range(self.num_layers)]
+        dec = [None for i in range(self.num_layers)]
+        all_output = [None for i in range(self.iterations)]
+        x = self.pre_transform_conv_block(x)
+        e_i = 0
+        d_i = 0
+        for iteration in range(self.iterations):
+            for layer in range(self.num_layers):
+                if layer == 0:
+                    x_in = x
+                x_in = self.encoders[e_i][0](cat([x_in, x_in if dec[-1 - layer] is None else dec[-1 - layer]], dim=1))
+                enc[layer] = x_in
+                x_in = self.encoders[e_i][1](x_in)
+                e_i = e_i + 1
+            x_in = self.middles(x_in)
+            for layer in range(self.num_layers):
+                x_in = self.decoders[d_i][0](cat([x_in, enc[-1 - layer]], dim=1))
+                dec[layer] = x_in
+                x_in = self.decoders[d_i][1](x_in)
+                d_i = d_i + 1
+            all_output[iteration] = x_in
         if self.integrate:
-            #collection.append(shorctut)
-            print(len(collection))
-            x = concatenate(collection)
-            
-        #the last stage block is not reusable
-        x = self.conv_block(x, self.bachnorm_momentum, int(32*self.multiplier))
-        x = self.conv_block(x, self.bachnorm_momentum, int(32*self.multiplier))
-
-        outputs = Conv2D(self.num_classes, kernel_size=(1,1), strides=(1,1), activation='sigmoid', padding='valid') (x)       
-
-        model = Model(inputs=[inputs], outputs=[outputs])
-        
-        return model
-    
-    def build(self):
-        conv_layers,  deconv_layers, mid_layers = self.define_layers()
-        model = self.define_graph(conv_layers, mid_layers, deconv_layers)
-        return model
-        
-    def conv_block(self,x, bachnorm_momentum,filters=None,conv=None):
-        if conv is not None:
-            x = conv(x)
-        else:
-            x = Conv2D(filters, **self.conv_args)(x)
-        x = BatchNormalization(momentum=bachnorm_momentum)(x)
-        return x          
+            x_in = cat(all_output, dim=1)
+        x_in = self.post_transform_conv_block(x_in)
+        return x_in
