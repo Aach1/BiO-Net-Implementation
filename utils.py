e96c2143fc73b9fb13abcf0851b9a6db16a2929d
Aach1 <harshithjaiganji@gmail.com>
2025-11-06 02:11:44 +0530

    Refactor training and data augmentation methods

--- utils.py
+++ utils.py
@@ -1,317 +1,221 @@
+from torch.utils.data import DataLoader
+import sys
+import os
 import numpy as np
-import matplotlib.pyplot as plt
-from keras.preprocessing.image import ImageDataGenerator
-
-# Runtime data augmentation
-def get_augmented(
-    X_train, 
-    Y_train, 
-    X_val=None,
-    Y_val=None,
-    batch_size=32, 
-    seed=0, 
-    data_gen_args = dict(
-        rotation_range=10.,
-        #width_shift_range=0.02,
-        height_shift_range=0.02,
-        shear_range=5,
-        #zoom_range=0.3,
-        horizontal_flip=True,
-        vertical_flip=False,
-        fill_mode='constant'
-    )):
+from torch.nn import BCELoss
+from torch.optim import Adam
+from torch.optim.lr_scheduler import LambdaLR
+import torch
+from matplotlib import pyplot as plt
+import imgaug.augmenters as iaa
+from tqdm import tqdm
+from metrics import *
+from dataloader import BiONetDataset
+from model import BiONet
+from metrics import bce_dice_loss, iou, dice_coef, accuracy_score
 
 
-    # Train data, provide the same seed and keyword arguments to the fit and flow methods
-    X_datagen = ImageDataGenerator(**data_gen_args)
-    Y_datagen = ImageDataGenerator(**data_gen_args)
-    X_datagen.fit(X_train, augment=True, seed=seed)
-    Y_datagen.fit(Y_train, augment=True, seed=seed)
-    X_train_augmented = X_datagen.flow(X_train, batch_size=batch_size, shuffle=True, seed=seed)
-    Y_train_augmented = Y_datagen.flow(Y_train, batch_size=batch_size, shuffle=True, seed=seed)
-    
-    train_generator = zip(X_train_augmented, Y_train_augmented)
+device = torch.device('cuda:0')
 
-    if not (X_val is None) and not (Y_val is None):
-        # Validation data, no data augmentation, but we create a generator anyway
-        X_datagen_val = ImageDataGenerator(**data_gen_args)
-        Y_datagen_val = ImageDataGenerator(**data_gen_args)
-        X_datagen_val.fit(X_val, augment=True, seed=seed)
-        Y_datagen_val.fit(Y_val, augment=True, seed=seed)
-        X_val_augmented = X_datagen_val.flow(X_val, batch_size=batch_size, shuffle=True, seed=seed)
-        Y_val_augmented = Y_datagen_val.flow(Y_val, batch_size=batch_size, shuffle=True, seed=seed)
 
-        # combine generators into one which yields image and masks
-        val_generator = zip(X_val_augmented, Y_val_augmented)
-        
-        return train_generator, val_generator
+def train(args):
+    transforms = iaa.Sequential([
+    iaa.Fliplr(0.5),
+    iaa.Flipud(0.3),
+    iaa.Affine(rotate=(-25, 25), scale=(0.9, 1.1)),
+    iaa.GaussianBlur(sigma=(0, 1.0)),
+    iaa.AdditiveGaussianNoise(scale=(0, 0.03*255)),
+])
+
+    train_set = BiONetDataset(args.train_data, 'chestxray', batchsize=args.batch_size, steps=args.steps, transforms=transforms)
+    test_set = BiONetDataset(args.valid_data, 'chestxray', transforms=None)
+
+    train_loader = DataLoader(dataset=train_set, batch_size=args.batch_size, shuffle=False, drop_last=True,
+                              num_workers=0, pin_memory=True)
+    test_loader = DataLoader(dataset=test_set, batch_size=args.batch_size, shuffle=False, num_workers=0,
+                             pin_memory=True)
+
+    model = BiONet(iterations=args.iter,
+                   num_classes=args.num_class,
+                   num_layers=4,
+                   multiplier=args.multiplier,
+                   integrate=args.integrate).to(device).float()
+
+    criterion = BCELoss()
+    optimizer = Adam(params=model.parameters(), lr=args.lr, weight_decay=0.)
+
+    fcn = lambda step: 1. / (1. + args.lr_decay * step)
+    scheduler = LambdaLR(optimizer, lr_lambda=fcn)
+
+    print('model successfully built and compiled.')
+
+    if not os.path.isdir("checkpoints/" + args.exp):
+        os.mkdir("checkpoints/" + args.exp)
+
+    best_iou = 0.0
+    print('\nStart training...')
+    for epoch in range(args.epochs):
+        tot_loss = 0.0
+        tot_iou = 0.0
+        tot_dice = 0.0
+        val_loss = 0.0
+        val_iou = 0.0
+        val_dice = 0.0
+
+        model.train()
+        for step, (x, y) in enumerate(
+                tqdm(train_loader, desc='[TRAIN] Epoch ' + str(epoch + 1) + '/' + str(args.epochs))):
+            if step >= args.steps:
+                break
+            x = x.to(device).float()
+            y = y.to(device).float()
+
+            optimizer.zero_grad()
+            output = model(x)
+
+            l = criterion(output, y)
+            tot_loss += l.item()
+            l.backward()
+            optimizer.step()
+
+            iou_score = iou(y, x)
+            dice_score = dice_coef(y, x)
+            tot_iou += iou_score
+            tot_dice += dice_score
+
+            scheduler.step()
+
+        print('[TRAIN] Epoch: ' + str(epoch + 1) + '/' + str(args.epochs),
+              'loss:', tot_loss / args.steps,
+              'iou:', tot_iou / args.steps,
+              'dice:', tot_dice / args.steps)
+
+        model.eval()
+        with torch.no_grad():
+            for step, (x, y) in enumerate(
+                    tqdm(test_loader, desc='[VAL] Epoch ' + str(epoch + 1) + '/' + str(args.epochs))):
+                x = x.to(device).float()
+                y = y.to(device).float()
+
+                output = model(x)
+
+                l = criterion(output, y)
+                val_loss += l.item()
+
+                x, y = output.detach().cpu().numpy(), y.cpu().numpy()
+                iou_score = iou(y, x)
+                dice_score = dice_coef(y, x)
+                val_iou += iou_score
+                val_dice += dice_score
+
+        if val_iou / len(test_loader) > best_iou:
+            best_iou = val_iou / len(test_loader)
+            save_model(args, model)
+
+        print('[VAL] Epoch: ' + str(epoch + 1) + '/' + str(args.epochs),
+              'val_loss:', val_loss / len(test_loader),
+              'val_iou:', val_iou / len(test_loader),
+              'val_dice:', val_dice / len(test_loader),
+              'best val_iou:', best_iou)
+
+    print('\nTraining fininshed!')
+
+
+def evaluate(args):
+    test_set = BiONetDataset(args.valid_data, "chestxray")
+    test_loader = DataLoader(dataset=test_set, batch_size=args.batch_size, shuffle=False, num_workers=0,
+                             pin_memory=True)
+
+    if args.model_path is None:
+        integrate = '_int' if args.integrate else ''
+        weights = '_weights'
+        cpt_name = 'iter_' + str(args.iter) + '_mul_' + str(args.multiplier) + integrate + '_best' + weights + '.pt'
+        model_path = "checkpoints/" + args.exp + "/" + cpt_name
     else:
-        return train_generator
+        model_path = args.model_path
+    print('Restoring model from path: ' + model_path)
+    model = BiONet(iterations=args.iter,
+                   num_classes=args.num_class,
+                   num_layers=4,
+                   multiplier=args.multiplier,
+                   integrate=args.integrate).to(device)
+    checkpoint = torch.load(model_path)
+    model.load_state_dict(checkpoint['state_dict'])
+
+    criterion = bce_dice_loss
+
+    val_loss = 0.0
+    val_iou = 0.0
+    val_dice = 0.0
+    val_acc = 0.0
+
+    segmentations = []
+
+    print('\nStart evaluation...')
+    model.eval()
+    with torch.no_grad():
+        for step, (x, y) in enumerate(tqdm(test_loader)):
+
+            x = x.to(device).float()
+            y = y.to(device).float()
+
+            output = model(x)
+
+            l = criterion(output, y)
+            val_loss += l.item()
+
+            iou_score = iou(output, y)
+            dice_score = dice_coef(output, y)
+            acc_score = accuracy_score(output, y)
+
+            val_iou += iou_score
+            val_dice += dice_score
+            val_acc += acc_score
+
+            if args.save_result:
+                segmentations.append(x)
+
+    val_loss /= len(test_loader)
+    val_iou /= len(test_loader)
+    val_dice /= len(test_loader)
+    val_acc /= len(test_loader)
+
+    print(f'Validation loss:\t{val_loss:.4f}')
+    print(f'Validation IoU:\t{val_iou:.4f}')
+    print(f'Validation Dice:\t{val_dice:.4f}')
+    print(f'Validation Accuracy:\t{val_acc:.4f}')
+
+    print('\nEvaluation finished!')
+
+    if args.save_result:
+
+        if not os.path.exists("checkpoints/" + args.exp + "/outputs"):
+            os.mkdir("checkpoints/" + args.exp + "/outputs")
+
+        with open("checkpoints/" + args.exp + "/outputs/result.txt", 'w+') as f:
+            f.write('Validation loss:\t' + str(val_loss) + '\n')
+            f.write('Validation  iou:\t' + str(val_iou) + '\n')
+            f.write('Validation dice:\t' + str(val_dice) + '\n')
+
+        print('Metrics have been saved to:', "checkpoints/" + args.exp + "/outputs/result.txt")
+
+        results = np.transpose(np.concatenate(segmentations, axis=0), (0, 2, 3, 1))
+        results = (results > 0.5).astype(np.float32)
+
+        print('Saving segmentations...')
+        if not os.path.exists("checkpoints/" + args.exp + "/outputs/segmentations"):
+            os.mkdir("checkpoints/" + args.exp + "/outputs/segmentations")
+
+        for i in range(results.shape[0]):
+            plt.imsave("checkpoints/" + args.exp + "/outputs/segmentations/" + str(i) + ".png", results[i, :, :, 0],
+                       cmap='gray')
+
+        print('A total of ' + str(results.shape[0]) + ' segmentation results have been saved to:',
+              "checkpoints/" + args.exp + "/outputs/segmentations/")
 
 
-def plot_segm_history(history, metrics=['iou', 'val_iou'], losses=['loss', 'val_loss']):
-    # summarize history for iou
-    plt.figure(figsize=(12,6))
-    for metric in metrics:
-        plt.plot(history.history[metric], linewidth=3)
-    plt.suptitle('metrics over epochs', fontsize=20)
-    plt.ylabel('metric', fontsize=20)
-    plt.xlabel('epoch', fontsize=20)
-    #plt.yticks(np.arange(0.3, 1, step=0.02), fontsize=35)
-    #plt.xticks(fontsize=35)
-    plt.legend(metrics, loc='center right', fontsize=15)
-    plt.show()
-    # summarize history for loss
-    plt.figure(figsize=(12,6))    
-    for loss in losses:
-        plt.plot(history.history[loss], linewidth=3)
-    plt.suptitle('loss over epochs', fontsize=20)
-    plt.ylabel('loss', fontsize=20)
-    plt.xlabel('epoch', fontsize=20)
-    #plt.yticks(np.arange(0, 0.2, step=0.005), fontsize=35)
-    #plt.xticks(fontsize=35)
-    plt.legend(losses, loc='center right', fontsize=15)
-    plt.show()
-
-
-def mask_to_red(mask):
-    '''
-    Converts binary segmentation mask from white to red color.
-    Also adds alpha channel to make black background transparent.
-    '''
-    img_size = mask.shape[0]
-    c1 = mask.reshape(img_size,img_size)
-    c2 = np.zeros((img_size,img_size))
-    c3 = np.zeros((img_size,img_size))
-    c4 = mask.reshape(img_size,img_size)
-    return np.stack((c1, c2, c3, c4), axis=-1)
-
-
-def mask_to_rgba(mask, color='red'):
-    '''
-    Converts binary segmentation mask from white to red color.
-    Also adds alpha channel to make black background transparent.
-    '''
-    img_size = mask.shape[0]
-    zeros = np.zeros((img_size,img_size))
-    ones = mask.reshape(img_size,img_size)
-    if color == 'red':
-        return np.stack((ones, zeros, zeros, ones), axis=-1)
-    elif color == 'green':
-        return np.stack((zeros, ones, zeros, ones), axis=-1)
-    elif color == 'blue':
-        return np.stack((zeros, zeros, ones, ones), axis=-1)
-    elif color == 'yellow':
-        return np.stack((ones, ones, zeros, ones), axis=-1)
-    elif color == 'magenta':
-        return np.stack((ones, zeros, ones, ones), axis=-1)
-    elif color == 'cyan':
-        return np.stack((zeros, ones, ones, ones), axis=-1)
-
-    
-def plot_imgs(org_imgs, 
-              mask_imgs, 
-              pred_imgs=None, 
-              nm_img_to_plot=10, 
-              figsize=4,
-              alpha=0.5
-             ):
-    '''
-    Image plotting for semantic segmentation data.
-    Last column is always an overlay of ground truth or prediction
-    depending on what was provided as arguments.
-    '''
-    if nm_img_to_plot > org_imgs.shape[0]:
-        nm_img_to_plot = org_imgs.shape[0]
-    im_id = 0
-    org_imgs_size = org_imgs.shape[1]
-
-    org_imgs = reshape_arr(org_imgs)
-    mask_imgs = reshape_arr(mask_imgs)
-    if  not (pred_imgs is None):
-        cols = 4
-        pred_imgs = reshape_arr(pred_imgs)
-    else:
-        cols = 3
-
-        
-    fig, axes = plt.subplots(nm_img_to_plot, cols, figsize=(cols*figsize, nm_img_to_plot*figsize))
-    axes[0, 0].set_title("original", fontsize=15) 
-    axes[0, 1].set_title("ground truth", fontsize=15)
-    if not (pred_imgs is None):
-        axes[0, 2].set_title("prediction", fontsize=15) 
-        axes[0, 3].set_title("overlay", fontsize=15) 
-    else:
-        axes[0, 2].set_title("overlay", fontsize=15) 
-    for m in range(0, nm_img_to_plot):
-        axes[m, 0].imshow(org_imgs[im_id], cmap=get_cmap(org_imgs))
-        axes[m, 0].set_axis_off()
-        axes[m, 1].imshow(mask_imgs[im_id], cmap=get_cmap(mask_imgs))
-        axes[m, 1].set_axis_off()        
-        if not (pred_imgs is None):
-            axes[m, 2].imshow(pred_imgs[im_id], cmap=get_cmap(pred_imgs))
-            axes[m, 2].set_axis_off()
-            axes[m, 3].imshow(org_imgs[im_id], cmap=get_cmap(org_imgs))
-            axes[m, 3].imshow(mask_to_red(zero_pad_mask(pred_imgs[im_id], desired_size=org_imgs_size)), cmap=get_cmap(pred_imgs), alpha=alpha)
-            axes[m, 3].set_axis_off()
-        else:
-            axes[m, 2].imshow(org_imgs[im_id], cmap=get_cmap(org_imgs))
-            axes[m, 2].imshow(mask_to_red(zero_pad_mask(mask_imgs[im_id], desired_size=org_imgs_size)), cmap=get_cmap(mask_imgs), alpha=alpha)
-            axes[m, 2].set_axis_off()
-        im_id += 1
-
-    plt.show()
-
-
-def zero_pad_mask(mask, desired_size):
-    pad = (desired_size - mask.shape[0]) // 2
-    padded_mask = np.pad(mask, pad, mode="constant")
-    return padded_mask
-
-
-def reshape_arr(arr):
-    if arr.ndim == 3:
-        return arr
-    elif arr.ndim == 4:
-        if arr.shape[3] == 3:
-            return arr
-        elif arr.shape[3] == 1:
-            return arr.reshape(arr.shape[0], arr.shape[1], arr.shape[2])
-
-
-def get_cmap(arr):
-    if arr.ndim == 3:
-        return 'gray'
-    elif arr.ndim == 4:
-        if arr.shape[3] == 3:
-            return 'jet'
-        elif arr.shape[3] == 1:
-            return 'gray'
-        
-
-def get_patches(img_arr, size=256, stride=256):
-    '''
-    Takes single image or array of images and returns
-    crops using sliding window method.
-    If stride < size it will do overlapping.
-    '''
-    # check size and stride
-    if size % stride != 0: 
-        raise ValueError('size % stride must be equal 0')
-        
-        
-    patches_list = []
-    overlapping = 0    
-    if stride != size:
-        overlapping = (size // stride) - 1
-    
-    if img_arr.ndim == 3:    
-        i_max = img_arr.shape[0] // stride-overlapping
-
-        for i in range(i_max):
-            for j in range(i_max):
-                #print(i*stride, i*stride+size)
-                #print(j*stride, j*stride+size)
-                patches_list.append(
-                    img_arr[i*stride:i*stride+size,
-                            j*stride:j*stride+size
-                           ])
-                
-    elif img_arr.ndim == 4:
-        i_max = img_arr.shape[1] // stride - overlapping
-        for im in img_arr:
-            for i in range(i_max):
-                for j in range(i_max):
-                    #print(i*stride, i*stride+size)
-                    #print(j*stride, j*stride+size)
-                    patches_list.append(
-                        im[i*stride:i*stride+size,
-                           j*stride:j*stride+size
-                          ])
-                    
-    else:
-        raise ValueError('img_arr.ndim must be equal 3 or 4')
-    
-    return np.stack(patches_list)
-        
-
-def plot_patches(img_arr, org_img_size, stride=None, size=None):
-    '''
-    Plots all the patches for the first image in 'img_arr' trying to reconstruct the original image
-    '''
-    
-    # check parameters
-    if type(org_img_size) is not tuple:
-        raise ValueError('org_image_size must be a tuple')
-        
-    if img_arr.ndim == 3:
-        img_arr = np.expand_dims(img_arr, axis=0)
-        
-    if size is None:
-        size = img_arr.shape[1]
-        
-    if stride is None:
-        stride = size
-            
-    i_max = (org_img_size[0] // stride) + 1 - (size // stride)
-    j_max = (org_img_size[1] // stride) + 1 - (size // stride)
-    
-    fig, axes = plt.subplots(i_max, j_max, figsize=(i_max*2, j_max*2))
-    fig.subplots_adjust(hspace=0.05, wspace=0.05)
-    jj = 0
-    for i in range(i_max):
-        for j in range(j_max):
-            axes[i, j].imshow(img_arr[jj])
-            axes[i, j].set_axis_off()
-            jj += 1
-
-########################
-
-def reconstruct_from_patches(img_arr, org_img_size, stride=None, size=None):
-    # check parameters
-    if type(org_img_size) is not tuple:
-        raise ValueError('org_image_size must be a tuple')
-        
-    if img_arr.ndim == 3:
-        img_arr = np.expand_dims(img_arr, axis=0)
-        
-    if size is None:
-        size = img_arr.shape[1]
-        
-    if stride is None:
-        stride = size
-        
-    nm_layers = img_arr.shape[3]
-    
-    i_max = (org_img_size[0] // stride) + 1 - (size // stride)
-    j_max = (org_img_size[1] // stride) + 1 - (size // stride)
-    
-    total_nm_images = (img_arr.shape[0]//(i_max**2))
-    nm_images = img_arr.shape[0]
-        
-    averaging_value = (size//stride)
-    images_list = []    
-    kk = 0
-    for img_count in range(total_nm_images):
-        img_bg = np.zeros((org_img_size[0], org_img_size[1], nm_layers), dtype=img_arr[0].dtype)
-        
-        for i in range(i_max):
-            for j in range(j_max):
-                for layer in range(nm_layers):
-                    img_bg[i*stride:i*stride+size, j*stride:j*stride+size, layer] = img_arr[kk,:,:,layer]
-                    
-                kk += 1        
-        # TODO add averaging for masks - right now it's just overwritting
-        
-#         for layer in range(nm_layers):
-#             # average some more because overlapping 4 patches
-#             img_bg[stride:i_max*stride, stride:i_max*stride, layer] //= averaging_value
-#             # corners:
-#             img_bg[0:stride, 0:stride, layer] *= averaging_value
-#             img_bg[i_max*stride:i_max*stride+stride, 0:stride, layer] *= averaging_value
-#             img_bg[i_max*stride:i_max*stride+stride, i_max*stride:i_max*stride+stride, layer] *= averaging_value
-#             img_bg[0:stride, i_max*stride:i_max*stride+stride, layer] *= averaging_value
-
-        images_list.append(img_bg)
-        
-    return np.stack(images_list)
+def save_model(args, model):
+    integrate = '_int' if args.integrate else ''
+    weights = '_weights'
+    cpt_name = 'iter_' + str(args.iter) + '_mul_' + str(args.multiplier) + integrate + '_best' + weights + '.pt'
+    torch.save({'state_dict': model.state_dict()}, "checkpoints/" + args.exp + "/" + cpt_name)
